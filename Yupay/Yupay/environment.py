c2bd316f318d01d147704bf2d5606c1d2a3d2cb5
mauricio.rodriguez <mauricio.rodriguez@utec.edu.pe>
2020-06-15 19:48:14 -0500

    stamping working

--- Yupay/Yupay/environment.py
+++ Yupay/Yupay/environment.py
@@ -2,4 +2,5 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-API_URL = os.getenv("URL_API_STAMP")
+POST_URL = os.getenv("URL_POST_STAMP")
+GET_URL = os.getenv("URL_GET_STAMP")
--- Yupay/stamp/views.py
+++ Yupay/stamp/views.py
@@ -14,13 +14,29 @@ def index(request): #after request you may include other parameters
 
 def post(request):
     try:
+        Token1 = env.ACCESS_TOKEN1
+        Token2 = env.ACCESS_TOKEN2
+        URL = env.POST_URL
+        params = {'evidence': 'Hola prueba desde back',
+                  'transactionType':'Stamping.io:API'}
+        headers = {
+                'Authorization': 'Basic {Token1}',
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic {Token2}'}
+        response = requests.post(URL,params = params, headers = headers)
+        return HttpResponse(response)   
+    except Exception as ex:
+        return HttpResponse(ex)
+    
+def get(request):
+    try:
         Token = env.ACCESS_TOKEN
-        URL = env.API_URL
+        URL = env.GET_URL
         dataTest = "evidence=Hola prueba"
         
         # auth=('Authorization','Basic'+Token)
         headers = {'Authorization': 'basic '+Token}
-        request = requests.post(URL+dataTest,headers = headers)
-        return HttpResponse(request)   
+        response = requests.request("POST",URL+dataTest,headers = headers)
+        return HttpResponse(response)   
     except Exception as ex:
         return HttpResponse(ex)
