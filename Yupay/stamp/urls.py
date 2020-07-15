3248776c4a673d04a75e3a52cef45b2e7a4a7e60
mauricio.rodriguez <mauricio.rodriguez@utec.edu.pe>
2020-07-15 17:27:40 -0500

    changeS

--- Yupay/stamp/urls.py
+++ Yupay/stamp/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls.static import static
 from django.conf import settings
 #all the urls in an app, like stamp(app)
 # must be pointed in the project urls file(yupay/urls)
-from .views  import index,forms,searchForm,search,search2
+from .views  import index,forms,searchForm,search
 from .views import stamping
 urlpatterns = [
     path('', index,name='index'),
     path('forms/', forms,name='forms'),
     path('searchform/', searchForm,name='searchForm'),
-    path('searchform/?userid=<int:id>', search,name='search'),
-    path('searchform/search/?<int:id>',search2,name='search'),
+    #path('searchform/?userid=<int:id>', search,name='search'),
+    path('searchform/search/',search,name='search'),
     # path('certificado/', certificados,name='certifado'),
     path('forms/stamp/', stamping.as_view(),name='stamp'),
     #    route, view, name, 
