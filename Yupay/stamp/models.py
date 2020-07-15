6df0e46fead53d9615b14454cd41ab1d9276c873
mauricio.rodriguez <mauricio.rodriguez@utec.edu.pe>
2020-07-14 22:59:39 -0500

    multiple changes in registering

--- Yupay/stamp/models.py
+++ Yupay/stamp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    DNI = models.IntegerField(primary_key=True) 
+    base_trxid = models.CharField(max_length=50) 
+    def getDNI(self):
+        return self.DNI
+    
+    def getTrx(self):
+        return self.base_trxid
--- Yupay/stamp/views.py
+++ Yupay/stamp/views.py
@@ -4,30 +4,31 @@ import json
 from django.http import HttpRequest
 from Yupay import environment as env
 from django.template import loader
-from django.template.loader import render_to_string
-from django.shortcuts import render
 import requests
 from django.http import JsonResponse
 from django.views import View
 from hashlib import sha256
-from django.views.decorators.csrf import csrf_exempt
 import base64
+from .models import Person
 # Create your views here. 
 # All the views must be called in the urls
+
+
+
 def index(request): #after request you may include other parameters
     # template = loader.get_template('root/file.html')
     try:
-        #return HttpResponse(template.render(response,request))
         return render(request,'index.html')
     except Exception as ex:
         return HttpResponse(ex)
-    
+
 def forms(request): 
     try:
         return render(request,'forms.html')
     except Exception as ex:
         return HttpResponse(ex)
-    
+
+        
 class stamping(View):
     def post(self,request,*args, **kwargs):  
         if request.method == "POST":      
@@ -35,34 +36,44 @@ class stamping(View):
                 Token = env.ACCESS_TOKEN
                 URL = env.POST_URL
                 data = json.loads(request.body)
-                #inputPrueba = {'Id': '124124124'}
-                #summary = json.dumps(dict(inputPrueba, **data))
-                #h = sha256(summary.encode('utf-8'))
-                h = sha256(json.dumps(data).encode('utf-8'))
-                params = {"evidence": h.hexdigest(),
+                reference = None
+                response = None
+                headers = {
+                            'Authorization': f'Basic {Token}',
+                            'Content-Type': 'application/json'}
+                dni = data['user_id']
+                try:
+                    instance = Person.objects.get(DNI=dni)
+                    reference = instance.base_trxid
+                    h = sha256(json.dumps(data).encode('utf-8'))
+                    params = {"evidence": h.hexdigest(),
                         'transactionType':'Stamping.io:API',
                             'data': base64.b64encode(json.dumps(data).encode('utf-8')),
-                            'subject':'Asunto'}
-                # params lat long(num or string)
-                # url 
-                #reference = trxid 
-                headers = {
-                        'Authorization': f'Basic {Token}',
-                        'Content-Type': 'application/json'}
-                
-                response = requests.post(URL,params = params, headers = headers)
-                return HttpResponse(response)   
+                            'subject':'Added Data',
+                            'reference':reference}
+                    response = requests.post(URL,params = params, headers = headers)
+                except Exception as ex:
+                    h = sha256(json.dumps(data).encode('utf-8'))
+                    params = {"evidence": h.hexdigest(),
+                            'transactionType':'Stamping.io:API',
+                                'data': base64.b64encode(json.dumps(data).encode('utf-8')),
+                                'subject':'newUser'}
+                    
+                    response = requests.post(URL,params = params, headers = headers)
+                    newRow = Person(data['user_id'],json.loads(response.content)['trxid'])
+                    newRow.save()
+                return HttpResponse(response.content)  
             except Exception as ex:
                 print(ex)
-                return HttpResponse(ex)
+                return HttpResponse("error: "+str(ex))
             
         
     def get(self, request,*args, **kwargs):
         try:
             URL = env.GET_URL
-            params = {'byTrxid': '95f1165e976b1dfe14486a167a6b28b7e3444aa3'}
+            params = {'byTrxid': '722103692035932df24e5fe43a4fe81c01534e9d'}
             # params = {'byHash': 'insert hash here'}
             response = requests.post(URL,params = params)
-            return HttpResponse(response)   
+            return HttpResponse(response.content)   
         except Exception as ex:
             return HttpResponse(ex)
